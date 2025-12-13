95893c0fe6ae9c25446c664fb034aed054e8a2cb
Blocksix12 <phuocnguyen120705@gmail.com>
2025-12-13 13:47:57 +0700

    update giao dien voi fix bug

--- views/borrow_view.py
+++ views/borrow_view.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 from tkcalendar import DateEntry
 from controllers.borrow_controller import BorrowController
-from datetime import datetime
 
 
 class BorrowView(ttk.Frame):
